d84f93156c3a6dcb320fa898141f0ebeb77d03c6
Erik McClure <erikm@fundament.software>
2023-04-19 04:45:54 -0700

    Fix misnamed child_database entry

--- src/models/block.rs
+++ src/models/block.rs
@@ -323,7 +323,7 @@ pub enum Block {
     ChildDatabase {
         #[serde(flatten)]
         common: BlockCommon,
-        child_page: ChildDatabaseFields,
+        child_database: ChildDatabaseFields,
     },
     Embed {
         #[serde(flatten)]
@@ -484,7 +484,9 @@ impl Into<CreateBlock> for Block {
             Block::Toggle { toggle, .. } => CreateBlock::Toggle { toggle },
             Block::Code { code, .. } => CreateBlock::Code { code },
             Block::ChildPage { child_page, .. } => CreateBlock::ChildPage { child_page },
-            Block::ChildDatabase { child_page, .. } => CreateBlock::ChildDatabase { child_page },
+            Block::ChildDatabase { child_database, .. } => {
+                CreateBlock::ChildDatabase { child_database }
+            }
             Block::Embed { embed, .. } => CreateBlock::Embed { embed },
             Block::Image { image, .. } => CreateBlock::Image { image },
             Block::Video { video, .. } => CreateBlock::Video { video },
@@ -556,7 +558,7 @@ pub enum CreateBlock {
         child_page: ChildPageFields,
     },
     ChildDatabase {
-        child_page: ChildDatabaseFields,
+        child_database: ChildDatabaseFields,
     },
     Embed {
         embed: EmbedFields,
